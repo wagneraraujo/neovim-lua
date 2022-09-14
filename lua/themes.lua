0b33b22620ef4eb897504d172581987ff8ebacbc
dev <dev@devs-MacBook-Pro.local>
2022-09-13 23:47:13 -0300

    theme ocean next

--- lua/themes.lua
+++ lua/themes.lua
@@ -9,14 +9,21 @@
   theme_style = "dark",
   -- other config
 }) ]]
-require("github-theme").setup({
+--[[ require("github-theme").setup({
   theme_style = "light_colorblind",
   -- other config
-})
+}) ]]
 
 --[[ require('onedark').setup {
     style = 'darker'
 }
 require('onedark').load() ]]
 
+require('nordic').colorscheme({
+    underline_option = 'none',
+    italic = true,
+    italic_comments = false,
+    minimal_mode = false,
+    alternate_backgrounds = false
+})
 
